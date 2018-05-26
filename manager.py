73e8eb931c2ddfbff51cc384f5db62bd637ef9a7
mengjunwei <mengjunweicool@163.com>
2018-05-26 14:58:12 +0800

    add_CSRF_Session

--- manager.py
+++ manager.py
@@ -1,6 +1,8 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from redis import StrictRedis
+from flask_wtf.csrf import CSRFProtect
+from flask_session import Session
 
 
 class Config(object):
@@ -10,16 +12,26 @@ class Config(object):
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     REDIS_HOST = '127.0.0.1'
     REDIS_PORT = 6379
+    SESSION_TYPE = 'redis'
+    SESSION_REDIS = StrictRedis(host=REDIS_HOST, port=REDIS_PORT)
+    SESSION_USE_SIGNER = True
+    PERMANENT_SESSION_LIFETIME = 3600 * 24
 
 
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 redis_store = StrictRedis(host=Config.REDIS_HOST, port=Config.REDIS_PORT)
+CSRFProtect(app)
+Session(app)
+
 
 
 @app.route('/')
 def index():
+    redis_store.set('name', 'it')
+    name = redis_store.get('name')
+    print(name)
     return 'in index'
 
 if __name__ == '__main__':
