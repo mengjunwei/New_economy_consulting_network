f289ab6eaa57421a79406dec334a2291b63fd672
mengjunwei <mengjunweicool@163.com>
2018-05-29 09:01:52 +0800

    网页右上角用户显示前后端逻辑

--- info/modules/index/views.py
+++ info/modules/index/views.py
@@ -1,10 +1,21 @@
 from . import index_blue
-from flask import render_template, current_app
+from flask import render_template, current_app, session
+from info.models import User
 
 
 @index_blue.route('/')
 def index():
-    return render_template('news/index.html')
+    user_id = session.get('user_id', None)
+    user = None
+    if user_id:
+        try:
+            user = User.query.get(user_id)
+        except Exception as e:
+            current_app.logger.error(e)
+    content = {
+        'user':user
+    }
+    return render_template('news/index.html', content=content)
 
 
 @index_blue.route('/favicon.ico')
