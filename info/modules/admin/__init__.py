c9f3792f0cab3ab5713df529ef072f90c24c58b9
mengjunwei <mengjunweicool@163.com>
2018-06-05 17:29:54 +0800

    后台登录页面的简单实现

--- info/modules/admin/__init__.py
+++ info/modules/admin/__init__.py
@@ -4,3 +4,6 @@ from flask import Blueprint
 admin_blue = Blueprint('admin', __name__, url_prefix='/admin')
 
 from . import views
+
+# @admin_blue.before_request()
+# def is_admin():
--- info/modules/admin/views.py
+++ info/modules/admin/views.py
@@ -1,20 +1,59 @@
 from . import admin_blue
-from info.utils.common import user_login_data
-from flask import g, current_app, render_template, redirect, url_for, request
+from flask import current_app, render_template, redirect, url_for, request, session
+from info.models import User
 
 
-@admin_blue.route('/login', methods=['POST', 'GET'])
-@user_login_data
-def login():
+@admin_blue.route('/index')
+def index():
     '''
     
     :return: 
     '''
-    #1.判断用户是否处于登录状态
-    user = g.user
-    if not user:
-        return redirect(url_for('admin.login'))
+    return render_template('admin/index.html')
 
-    #2.若为get请求，则渲染界面
+
+@admin_blue.route('/login', methods=['POST', 'GET'])
+def login():
+    '''
+    #1.若为get请求，则渲染界面
+    2.若为post请求，则接受参数
+    3,校验参数
+    4.校验用户是否存在
+    5校验用户名和面是否正确
+    6.写入状态保持
+    :return: 跳转到登录页面
+    '''
+    #1.若为get请求，则渲染界面
     if request.method == 'GET':
         return render_template('admin/login.html')
+
+    #2.若为post请求，则接受参数
+    if request.method == 'POST':
+        password = request.form.get('password')
+        nick_name = request.form.get('username')
+
+        #3,校验参数
+        if not all([password, nick_name]):
+            return render_template('admin/login.html', errmsg='缺少参数')
+
+        #4.校验用户是否存在
+        try:
+            user = User.query.filter(User.nick_name == nick_name).first()
+        except Exception as e:
+            current_app.logger.error(e)
+            return render_template('admin/login.html', errmsg='查询用户失败')
+
+        #5校验用户名和面是否正确
+        if not user:
+            return render_template('admin/login.html', errmsg='用户名或密码错误')
+        if not user.check_passowrd(password):
+            return render_template('admin/login.html', errmsg='用户名或密码错误')
+
+        #6.写入状态保持
+        session['user_id'] = user.id
+        session['nick_name'] = user.nick_name
+        session['mobile'] = user.mobile
+        session['is_admin'] = user.is_admin
+
+        #7.跳转到首页
+        return redirect(url_for('admin.index'))
